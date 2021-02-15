6bfb9f17cec86f55ce4ba484c705321161178dce
Artur Caliendo Prado <artur.prado@gmail.com>
2021-02-15 16:15:18 -0300

    Use .env to manage database values

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module hq-collections.com
 
 go 1.13
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/joho/godotenv v1.3.0 // indirect
+	gorm.io/gorm v1.20.12 // indirect
+)
--- main.go
+++ main.go
@@ -4,11 +4,27 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 const basePath = "/api"
 
 func main() {
+	e := godotenv.Load() //Load .env file
+	if e != nil {
+		fmt.Print(e)
+	}
+
+	userName := os.Getenv("db_user")
+	password := os.Getenv("db_pass")
+	dbName := os.Getenv("db_name")
+	dbHost := os.Getenv("db_host")
+
+	fmt.Println(userName, password, dbName, dbHost)
+
 	SetupRoutes(basePath)
 	fmt.Println("\nWebserver is up and running on port 5000, waiting for connections...")
 	err := http.ListenAndServe(":5000", nil)
