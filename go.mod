6db261015ba08a40b3cae467983c5f007a04be17
Artur Caliendo Prado <artur.prado@gmail.com>
2021-02-16 09:12:45 -0300

    Ping the database

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.13
 
 require (
 	github.com/gorilla/mux v1.8.0
+	github.com/jinzhu/gorm v1.9.16 // indirect
 	github.com/joho/godotenv v1.3.0 // indirect
 	gorm.io/gorm v1.20.12 // indirect
 )
--- main.go
+++ main.go
@@ -7,10 +7,24 @@ import (
 
 	"os"
 
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"github.com/joho/godotenv"
 )
 
 const basePath = "/api"
+
+func connectionString() string {
+	userName := os.Getenv("db_user")
+	password := os.Getenv("db_pass")
+	dbName := os.Getenv("db_name")
+	dbHost := os.Getenv("db_host")
+	dbPort := os.Getenv("db_port")
+
+	fmt.Println(userName, password, dbName, dbHost)
+
+	return fmt.Sprintf("host=%v port=%v user=%v dbname=%v password=%v sslmode=disable", dbHost, dbPort, userName, dbName, password)
+}
 
 func main() {
 	e := godotenv.Load() //Load .env file
@@ -18,16 +32,24 @@ func main() {
 		fmt.Print(e)
 	}
 
-	userName := os.Getenv("db_user")
-	password := os.Getenv("db_pass")
-	dbName := os.Getenv("db_name")
-	dbHost := os.Getenv("db_host")
+	db, err := gorm.Open("postgres", connectionString())
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
 
-	fmt.Println(userName, password, dbName, dbHost)
+	database := db.DB()
+
+	err = database.Ping()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Println("Connection to the database succeeded")
 
 	SetupRoutes(basePath)
 	fmt.Println("\nWebserver is up and running on port 5000, waiting for connections...")
-	err := http.ListenAndServe(":5000", nil)
+	err = http.ListenAndServe(":5000", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
